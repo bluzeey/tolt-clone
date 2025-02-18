ce1ecde9feb7b3f1a74a5b4fb1f4ce3ea3bb171a
Sahil Maheshwari <sahilm1711@gmail.com>
2025-02-18 08:50:20 +0530

    Create Clients.tsx

---
+++ frontend/src/components/Clients.tsx
@@ -0,0 +1,62 @@
+export default function Clients() {
+  return (
+    <div>
+      <h3>Backed by leading investors</h3>
+      <div className="w-layout-grid home-logo-grid">
+        <div className="home-logo-hero-item">
+          <a
+            href="https://musicfy.lol/"
+            target="_blank"
+            className="w-inline-block"
+          >
+            <img
+              src="https://cdn.prod.website-files.com/6329ec26d5d4133a1b0ed116/65b9619ebc9a36f158fe6e78_musicfylogo.svg"
+              loading="lazy"
+              alt="Musicfy Logo"
+              className="home-hero-item-image"
+            />
+          </a>
+        </div>
+        <div className="home-logo-hero-item">
+          <img
+            src="https://cdn.prod.website-files.com/6329ec26d5d4133a1b0ed116/65c4b370a7ebcb4fa9ffad6d_6577ac86d3e376c4d5f8243e_logo.svg"
+            loading="lazy"
+            alt="Speak.com Logo"
+            height=""
+            className="home-hero-item-image"
+          />
+        </div>
+        <div className="home-logo-hero-item">
+          <img
+            src="https://cdn.prod.website-files.com/6329ec26d5d4133a1b0ed116/6454d0ce8ef8f62be67dc36c_61c0debf73a9933e580459cc_logo%20(1).svg"
+            loading="lazy"
+            alt="Practice.do Logo"
+            className="home-hero-item-image"
+          />
+        </div>
+        <div className="home-logo-hero-item">
+          <img
+            src="https://cdn.prod.website-files.com/6329ec26d5d4133a1b0ed116/6529423aace0c95f08e2472e_logo-on-light-200x52.svg"
+            loading="lazy"
+            alt="Creately Logo"
+            className="home-hero-item-image"
+          />
+        </div>
+        <div className="home-logo-hero-item" />
+        <a
+          id="w-node-_77872ba7-f32f-388c-5672-947c33897619-71f1dac3"
+          href="https://persuva.ai/"
+          target="_blank"
+          className="w-inline-block"
+        >
+          <img
+            src="https://cdn.prod.website-files.com/6329ec26d5d4133a1b0ed116/6530f0d80bb49d25a59bd112_652a6ac279cbf4ed07e3c38d_persuvalogo%20(1).svg"
+            loading="lazy"
+            alt="Persuva Logo"
+            className="home-hero-item-image"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
