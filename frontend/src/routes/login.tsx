0e09931f9c807dab5e892ec90089c4e607e42b1e
Sahil Maheshwari <sahilm1711@gmail.com>
2025-02-18 22:39:26 +0530

    Removing login

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
 import LoginForm from "../components/Login/Form";
 
 export const Route = createFileRoute("/login")({
